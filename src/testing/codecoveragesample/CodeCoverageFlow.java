4779d09415e008fbc6616f6bbcd0cb8e73cf9a0e
deathdayss <45457242+deathdayss@users.noreply.github.com>
2024-03-02 13:16:45 +1100

    Add code coverage flow sample code

---
+++ src/testing/codecoveragesample/CodeCoverageFlow.java
@@ -0,0 +1,18 @@
+package testing.codecoveragesample;
+
+public class CodeCoverageFlow {
+    public void someMethod(boolean a, boolean b, boolean c) {
+        if (a) {
+            // statement X
+        }
+        else {
+            // statement Y
+            if (b) {
+                // statement Z
+            }
+        }
+        if (c) {
+            // statement W
+        }
+    }
+}
