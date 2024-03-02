bb6e5fa44ce5be144f5284c93c39af02758f7960
deathdayss <45457242+deathdayss@users.noreply.github.com>
2024-03-02 14:03:36 +1100

    Add CodeCoverageQuiz

---
+++ src/testing/p3/CodeCoverageQuiz.java
@@ -0,0 +1,19 @@
+package testing.p3;
+
+public class CodeCoverageQuiz {
+    public void workMethod(int a, int b) {
+        if (a > b) {
+            if (a > 1) {
+                System.out.println(a);
+                a = b + 1;
+            }
+            System.out.println(b);
+        }
+        for (int i = 0; i < 2; i++) {
+            if (a > b) {
+                a++;
+            }
+            System.out.println(a);
+        }
+    }
+}
